4c941bb7735b253e43e6ad2a286d996baef023d0
Ali Tashakori <ali.tashakori.23@gmail.com>
2023-05-19 19:54:43 +0330

    finish

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 
 final Color? backgroundGrey = Colors.grey[900];
 final Color? textWhite = Colors.white;
+final Color? textRed = Colors.red;
+final Color? textGreen = Colors.green;
+final Color? textAmber = Colors.amber;
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_tic_tac_toe/constants/colors.dart';
 
@@ -12,30 +10,57 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   bool isturn = true;
+  bool finish = false;
+
   List<String> XorOList = ['', '', '', '', '', '', '', '', ''];
-  var Turn = '';
+
+  String Turn = '';
+  String winner = '';
+
+  int equal = 0;
+  int ScoreX = 0;
+  int ScoreO = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundGrey,
       appBar: AppBar(
-        title: Text(
-          'Tic Toc Toe',
-          style: TextStyle(fontSize: 30),
-        ),
         centerTitle: true,
         backgroundColor: backgroundGrey,
         elevation: 0,
+        title: Text(
+          'Tic Toc Toe',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: textAmber,
+          ),
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: IconButton(
+              onPressed: () {
+                ClearGame();
+                ScoreO = 0;
+                ScoreX = 0;
+              },
+              icon: Icon(Icons.replay),
+              splashRadius: 20,
+            ),
+          ),
+        ],
       ),
       body: SafeArea(
         child: Column(
           children: [
             SizedBox(
-              height: 15.0,
+              height: 5.0,
             ),
             getScoreboard(),
             getGridView(),
+            getResultButton(),
             getTurn(),
             SizedBox(
               height: 24.0,
@@ -46,57 +71,69 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  Widget getResultButton() {
+    return Visibility(
+      visible: finish,
+      child: OutlinedButton(
+        onPressed: () {
+          ClearGame();
+        },
+        child: Text(
+          '${winner}play again!',
+          style: TextStyle(
+            color: textWhite,
+            fontSize: 25,
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget getScoreboard() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Text(
-                'Player O',
+        Column(
+          children: [
+            Text(
+              'Player O',
+              style: TextStyle(
+                color: textRed,
+                fontSize: 25,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '$ScoreO',
                 style: TextStyle(
                   color: textWhite,
                   fontSize: 25,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  '8',
-                  style: TextStyle(
-                    color: textWhite,
-                    fontSize: 25,
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Text(
-                'Player X',
+        Column(
+          children: [
+            Text(
+              'Player X',
+              style: TextStyle(
+                color: textGreen,
+                fontSize: 25,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '$ScoreX',
                 style: TextStyle(
                   color: textWhite,
                   fontSize: 25,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  '5',
-                  style: TextStyle(
-                    color: textWhite,
-                    fontSize: 25,
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ],
     );
@@ -105,16 +142,20 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget getGridView() {
     return Expanded(
       child: Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(20.0),
         child: GridView.builder(
           itemCount: 9,
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 3,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
           ),
           itemBuilder: (context, index) {
             return GestureDetector(
               onTap: () {
-                onTap(index);
+                if (!finish) {
+                  onTap(index);
+                }
               },
               child: Container(
                 height: 100,
@@ -123,8 +164,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Text(
                     XorOList[index],
                     style: TextStyle(
-                        color:
-                            XorOList[index] == 'X' ? Colors.green : Colors.red,
+                        color: XorOList[index] == 'X' ? textGreen : textRed,
                         fontSize: 70,
                         fontWeight: FontWeight.bold),
                   ),
@@ -145,25 +185,148 @@ class _HomeScreenState extends State<HomeScreen> {
       if (XorOList[index] == '') {
         if (isturn) {
           XorOList[index] = 'O';
+          equal++;
         } else {
           XorOList[index] = 'X';
+          equal++;
         }
         isturn = !isturn;
+
+        CheckWinner();
       }
+    });
+  }
+
+  void CheckWinner() {
+    if (XorOList[0] == XorOList[1] &&
+        XorOList[0] == XorOList[2] &&
+        XorOList[0] != '') {
+      ResultGame(XorOList[0]);
+      finish = true;
+      if (XorOList[0] == 'X') {
+        ScoreX++;
+      } else {
+        ScoreO++;
+      }
+      return;
+    }
+    if (XorOList[3] == XorOList[4] &&
+        XorOList[3] == XorOList[5] &&
+        XorOList[3] != '') {
+      ResultGame(XorOList[3]);
+      finish = true;
+      if (XorOList[3] == 'X') {
+        ScoreX++;
+      } else {
+        ScoreO++;
+      }
+      return;
+    }
+    if (XorOList[6] == XorOList[7] &&
+        XorOList[6] == XorOList[8] &&
+        XorOList[6] != '') {
+      ResultGame(XorOList[6]);
+      finish = true;
+      if (XorOList[6] == 'X') {
+        ScoreX++;
+      } else {
+        ScoreO++;
+      }
+      return;
+    }
+    if (XorOList[0] == XorOList[3] &&
+        XorOList[0] == XorOList[6] &&
+        XorOList[0] != '') {
+      ResultGame(XorOList[0]);
+      finish = true;
+      if (XorOList[0] == 'X') {
+        ScoreX++;
+      } else {
+        ScoreO++;
+      }
+      return;
+    }
+    if (XorOList[1] == XorOList[4] &&
+        XorOList[1] == XorOList[7] &&
+        XorOList[1] != '') {
+      ResultGame(XorOList[1]);
+      finish = true;
+      if (XorOList[1] == 'X') {
+        ScoreX++;
+      } else {
+        ScoreO++;
+      }
+      return;
+    }
+    if (XorOList[2] == XorOList[5] &&
+        XorOList[2] == XorOList[8] &&
+        XorOList[2] != '') {
+      ResultGame(XorOList[2]);
+      finish = true;
+      if (XorOList[2] == 'X') {
+        ScoreX++;
+      } else {
+        ScoreO++;
+      }
+      return;
+    }
+    if (XorOList[2] == XorOList[4] &&
+        XorOList[2] == XorOList[6] &&
+        XorOList[2] != '') {
+      ResultGame(XorOList[2]);
+      finish = true;
+      if (XorOList[2] == 'X') {
+        ScoreX++;
+      } else {
+        ScoreO++;
+      }
+      return;
+    }
+    if (XorOList[0] == XorOList[4] &&
+        XorOList[0] == XorOList[8] &&
+        XorOList[0] != '') {
+      ResultGame(XorOList[0]);
+      finish = true;
+      if (XorOList[0] == 'X') {
+        ScoreX++;
+      } else {
+        ScoreO++;
+      }
+      return;
+    }
+    if (equal == 9) {
+      setState(() {
+        winner = 'Draw , ';
+      });
+    }
+  }
+
+  void ClearGame() {
+    setState(() {
+      finish = false;
+      XorOList = ['', '', '', '', '', '', '', '', ''];
+      winner = '';
+      equal = 0;
+    });
+  }
+
+  void ResultGame(String win) {
+    setState(() {
+      winner = 'Winner is $win , ';
     });
   }
 
   Widget getTurn() {
     if (isturn) {
-      Turn = 'Turn O';
+      Turn = '<< Turn O >>';
     } else {
-      Turn = 'Turn X';
+      Turn = '<< Turn X >>';
     }
     return Text(
       Turn,
       style: TextStyle(
         fontSize: 30.0,
-        color: textWhite,
+        color: textAmber,
         fontWeight: FontWeight.bold,
       ),
     );
